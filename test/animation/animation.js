1c8844995fec369b3f118ef78c4786723edecc7a
Santiago Sojka <80698096+sojkaa@users.noreply.github.com>
2024-10-04 09:03:30 -0300

    Delete test directory

--- test/animation/animation.js
+++
@@ -1,9 +0,0 @@
-const infoBlock = document.querySelector('.info-block');
-
-infoBlock.addEventListener('mouseover', () => {
-  infoBlock.classList.add('show');
-});
-
-infoBlock.addEventListener('mouseout', () => {
-  infoBlock.classList.remove('show');
-});
